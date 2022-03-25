29310de35564215364dbeff0322effdb84272a2a
tyanotyrano <tyrano09@gmail.com>
2022-03-25 14:12:20 +0900

    refactor(AppConfig.java): 코드 중복 제거 및 메소드 추출 적용
    코드 중복 제거 및 메소드 추출을 적용한다.
    - AppConfig : 코드 중복 제거 및 메소드 추출을 적용

--- src/main/java/com/tyranotyrano/config/AppConfig.java
+++ src/main/java/com/tyranotyrano/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.tyranotyrano.config;
 
+import com.tyranotyrano.domain.discount.DiscountPolicy;
 import com.tyranotyrano.domain.discount.FixDiscountPolicy;
+import com.tyranotyrano.domain.member.repository.MemberRepository;
 import com.tyranotyrano.domain.member.repository.MemoryMemberRepository;
 import com.tyranotyrano.service.MemberService;
 import com.tyranotyrano.service.MemberServiceImpl;
@@ -10,10 +12,18 @@ import com.tyranotyrano.service.OrderServiceImpl;
 public class AppConfig {
     
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
