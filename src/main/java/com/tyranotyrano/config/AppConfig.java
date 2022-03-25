b7fdfcd0f9c8fdfb8cd99fd03d9fe497747c557a
tyanotyrano <tyrano09@gmail.com>
2022-03-25 17:32:43 +0900

    feat(AppConfig.java): AppConfig 를 스프링 프레임워크로 전환
    AppConfig 를 스프링 프레임워크로 전환한다.
    - AppConfig : AppConfig 를 스프링 프레임워크로 전환
    - MemberApp : ApplicationContext 에서 직접 Bean 을 가져와 MemberService 관련 로직 실행 테스트

--- src/main/java/com/tyranotyrano/config/AppConfig.java
+++ src/main/java/com/tyranotyrano/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.tyranotyrano.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.tyranotyrano.domain.discount.DiscountPolicy;
 import com.tyranotyrano.domain.discount.FixDiscountPolicy;
 import com.tyranotyrano.domain.discount.RateDiscountPolicy;
@@ -10,21 +13,26 @@ import com.tyranotyrano.service.MemberServiceImpl;
 import com.tyranotyrano.service.OrderService;
 import com.tyranotyrano.service.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
-    
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         // 1. 정액 할인 정책
         //return new FixDiscountPolicy();
 
