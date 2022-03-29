a3b91fe097773673c6803a8d8c12b153fb6db983
tyanotyrano <tyrano09@gmail.com>
2022-03-29 14:02:57 +0900

    test(SingletonTest.java): 스프링 컨테이너의 싱글톤 패턴 테스트 코드 작성
    스프링 컨테이너의 싱글톤 패턴 테스트 코드를 작성한다.
    - SingletonTest : 스프링 컨테이너의 싱글톤 패턴 테스트 코드 작성

--- src/test/java/com/tyranotyrano/singleton/SingletonTest.java
+++ src/test/java/com/tyranotyrano/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package com.tyranotyrano.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.tyranotyrano.config.AppConfig;
 import com.tyranotyrano.service.MemberService;
@@ -36,5 +38,21 @@ public class SingletonTest {
 
         // same : == 비교, equals : Objects.equals() 비교
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
