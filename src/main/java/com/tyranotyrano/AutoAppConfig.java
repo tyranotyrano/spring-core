ada87276e0dc6affca85946069dcae520369acbe
tyanotyrano <tyrano09@gmail.com>
2022-03-31 16:33:21 +0900

    refactor(AutoAppConfig.java): AutoAppConfig 파일 위치 이동
    AutoAppConfig 파일 위치를 전체 패키지가 스캔되도록 이동한다.

--- src/main/java/com/tyranotyrano/AutoAppConfig.java
+++ src/main/java/com/tyranotyrano/AutoAppConfig.java
@@ -1,4 +1,4 @@
-package com.tyranotyrano.config;
+package com.tyranotyrano;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
