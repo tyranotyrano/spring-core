f434017227dda06a9d2d11fef9dd30b9fc67b080
tyanotyrano <tyrano09@gmail.com>
2022-04-01 12:47:49 +0900

    feat(AutoAppConfig.java): 같은 이름으로 빈 등록되도록 설정 추가 및 주석처리
    같은 이름으로 빈 등록되도록 설정 추가 및 주석처리 한다.
    - AutoAppConfig : memoryMemberRepository 이름으로 빈 등록 추가
    - application.properties : 같은 이름으로 빈 등록되도록 설정 추가 및 주석처리

--- src/main/java/com/tyranotyrano/config/AutoAppConfig.java
+++ src/main/java/com/tyranotyrano/config/AutoAppConfig.java
@@ -1,8 +1,12 @@
 package com.tyranotyrano.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+
+import com.tyranotyrano.domain.member.repository.MemberRepository;
+import com.tyranotyrano.domain.member.repository.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -12,4 +16,10 @@ import org.springframework.context.annotation.FilterType;
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 중복 이름으로 빈 생성을 위해 임시로 넣음
+    /*@Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }*/
 }
