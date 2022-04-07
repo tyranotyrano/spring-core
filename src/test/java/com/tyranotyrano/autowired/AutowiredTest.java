4f67abc6bf873f80928cbf8a927fae7b570852a3
tyanotyrano <tyrano09@gmail.com>
2022-04-08 08:59:53 +0900

    test(AutowiredTest.java): Nullable 한 빈 주입 테스트 코드 작성
    Nullable 한 빈 주입 테스트 코드를 작성한다.
    - AutowiredTest : Nullable 한 빈 주입 테스트 코드 작성

---
+++ src/test/java/com/tyranotyrano/autowired/AutowiredTest.java
@@ -0,0 +1,41 @@
+package com.tyranotyrano.autowired;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import com.tyranotyrano.domain.member.Member;
+
+public class AutowiredTest {
+
+    @Test
+    void autowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+        /**
+         * 아래 3개 메소드는 모두 빈 주입이 안되기 때문에
+         * 빈이 아닌 Member 를 인자로 테스트를 진행해도 문제 없다.
+         * */
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
